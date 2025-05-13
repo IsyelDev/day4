56de37d5c8f02688bd5bac39a39722575328cf8f
=manuel <=emdelapazt@gmail.com>
2025-05-12 23:30:12 -0500

    iterable

--- science/index.py
+++ science/index.py
@@ -52,5 +52,63 @@ for i in range(0, len(notas_grupo), 4):
 print(nombres_alumnosa)
 print(notas_separadasa)
 
+
+datos = ["Juan", 25, "Lucía", 30, "Carlos", 22, "Ana", 28]
+
+# Tu objetivo:
+# nombres = ["Juan", "Lucía", "Carlos", "Ana"]
+# edades = [25, 30, 22, 28]
+def descomponer(datos):
+    nombres=[]
+    edades=[]
+    for i in range(0,len(datos),2):
+        nombres.append([datos[i]])
+        edades.append([datos[i+1]])
+    return nombres , edades
+
+print("="*40)
+nombres , edades = descomponer(datos)
+print(nombres)
+print(edades)
+
+
+lista = ["Pan", 1.2, "Leche", 0.99, "Queso", 2.5, "Jugo", 1.8]
+
+# Resultado esperado:
+# productos = ["Pan", "Leche", "Queso", "Jugo"]
+# precios = [1.2, 0.99, 2.5, 1.8]
+
+def productos(lista):
+    x=[]
+    y=[]
+    for i in range(0,len(lista),2):
+        x.append(lista[i]) 
+        y.append(lista[i+1])
+    return x , y
+
+producto , precio = productos(lista)
+print(producto)
+print(precio)
+
+
+materias = ["María", 7, 8, 9, 10, "Pedro", 6, 6, 7, 8, "Julia", 9, 9, 10, 10]
+
+# Resultado esperado:
+# alumnos = ["María", "Pedro", "Julia"]
+# promedios = [8.5, 6.75, 9.5]
+def promedio(materias):
+    alumnos=[]
+    promedios=[]
+    
+    for i in range(0, len(materias),5):
+        alumnos.append(materias[i])
+        notas.append(materias[i+1:i+5])
+    
+    for nota in notas:
+        promedios.append(sum(nota)/len(nota))
+    
+    return alumnos , notas , promedios
+
+print(promedio(materias))
 if __name__=="__main__":
-    print(genera_numero())
+    print(f"Soy un numero generado {genera_numero()}")
